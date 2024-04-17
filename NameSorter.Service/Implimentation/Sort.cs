5106b11ffbc445d21308040cc0a53d813a1da15b
Joel KAPUKU <joelkapuku@gmail.com>
2024-04-17 15:17:44 -0400

    [Feat: complete]: finalize sort implimentation in sort service also added a signature implimentation of saveOrderedNameToNewFile

--- NameSorter.Service/Implimentation/Sort.cs
+++ NameSorter.Service/Implimentation/Sort.cs
@@ -2,10 +2,41 @@
 {
     public class Sort : ISort
     {
-        public void SortByLastNameThenGivenNames(string name)
+        public List<NameModel> SortByLastNameThenGivenNames(string namesString)
         {
-            Console.WriteLine(name);
-            //throw new NotImplementedException();
+            List<NameModel> models = new();
+            List<string> namesList = ConvertStringToList(namesString);
+
+            foreach (var name in namesList)
+            {
+                string lastName = name.Substring(name.LastIndexOf(' ') + 1);
+                string givenName = name.Substring(0, name.LastIndexOf(" "));
+
+                models.Add(CreateNewNameModel(lastName, givenName));
+            }
+
+            return models.OrderBy(name=> name.LastName).ToList();
+        } 
+
+        private static List<string> ConvertStringToList(string namesString)
+        {
+            string[] namesArray = namesString.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            return new List<string>(namesArray);
+        }
+
+        private NameModel CreateNewNameModel(string lastName, string givenName)
+        {
+            return new NameModel()
+            {
+                LastName = lastName,
+                GivenNames = givenName
+            };
+        }
+
+        public void SaveOrderedNamesToNewFile(List<NameModel> names)
+        {
+            throw new NotImplementedException();
         }
     }
 }
