1c52e71869c122163e8cea02b267982ece3cffca
Joel KAPUKU <joelkapuku@gmail.com>
2024-04-17 02:31:12 -0400

    [Feat]: read name from unsorted-names-list.txt and display to console

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-        } 
+            string data;
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new("C:\\unsorted-names-list.txt");
+                data = streamReader.ReadLine();
+
+                while (data != null)
+                {
+                    Console.WriteLine(data);
+                    data = streamReader.ReadLine();
+                }
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                streamReader.Close();
+            }
+        }
     }
 }
